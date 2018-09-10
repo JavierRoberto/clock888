27b211410c0d59b5265d51453ae3f6d421a508c1
Javier Roberto <javierrr29@gmail.com>
2018-09-10 20:59:14 +0200

    Initial Commit

---
+++ clock888/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clock888
+//
+//  Created by Javier Roberto on 10/09/2018.
+//  Copyright © 2018 funtastic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
