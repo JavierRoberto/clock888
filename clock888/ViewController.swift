9afd0bf7ee3276baa65abc15d9ec0251b511facc
Javier Roberto <javierrr29@gmail.com>
2018-09-11 00:33:15 +0200

    behaviour done

--- clock888/ViewController.swift
+++ clock888/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var startTime: Double = 0
+    var time: Double = 0
+    var timer: Timer?
+    var isFirstTime: Bool = true
+    
     lazy var clockView: UIView = {
         let view = UIView(frame: .zero)
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -24,22 +29,85 @@ class ViewController: UIViewController {
         return label
     }()
     
+    lazy var button: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .green
+        button.setTitle("title", for: UIControl.State.normal)
+        button.addTarget(self, action: #selector(buttonAction), for: UIControl.Event.touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupView()
         setupConstraints()
-
     }
     
     func setupView() {
         view.addSubview(clockView)
         clockView.addSubview(clockLabel)
+        view.addSubview(button)
+        
+        clockView.layer.cornerRadius = 12
+        clockView.clipsToBounds = true
     }
     
     func setupConstraints() {
         clockLabel.fit(to: clockView, insets: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
-        clockView.center(into: view)
+        button.center(into: view)
+        NSLayoutConstraint.activate([
+            clockView.heightAnchor.constraint(equalToConstant: 100),
+            clockView.widthAnchor.constraint(equalToConstant: 250),
+            clockView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            clockView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            
+            button.heightAnchor.constraint(equalToConstant: 60),
+            button.widthAnchor.constraint(equalToConstant: 60)
+            ])
+    }
+    
+    func startTimer(){
+        startTime = Date().timeIntervalSinceReferenceDate
+        timer = Timer.scheduledTimer(timeInterval: 0.05,
+                             target: self,
+                             selector: #selector(advanceTimer),
+                             userInfo: nil,
+                             repeats: true)
+    }
+    
+    @objc func advanceTimer() {
+        time = Date().timeIntervalSinceReferenceDate - startTime
+        clockLabel.text = "\(time.minuteSecondMS)"
+    }
+    
+    @objc func buttonAction() {
+        isFirstTime ? initTime() : stopTime()
+    }
+    
+    func initTime() {
+        isFirstTime = false
+        startTimer()
+    }
+    
+    func stopTime() {
+        isFirstTime = true
+        timer?.invalidate()
+        
     }
 }
 
+extension TimeInterval {
+    var minuteSecondMS: String {
+        return String(format:"%d:%02d.%03d", minute, second, millisecond)
+    }
+    var minute: Int {
+        return Int((self/60).truncatingRemainder(dividingBy: 60))
+    }
+    var second: Int {
+        return Int(self.truncatingRemainder(dividingBy: 60))
+    }
+    var millisecond: Int {
+        return Int((self*1000).truncatingRemainder(dividingBy: 1000) )
+    }
+}
